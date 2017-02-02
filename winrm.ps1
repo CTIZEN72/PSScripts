01eb5e47d5f61bf5f545ea1e1e6dc31bcc454b49
aRKei93 <ruslan.khan93@gmail.com>
2017-02-02 20:41:11 +0500

    Add files via upload

--- winrm.ps1
+++ winrm.ps1
@@ -1,26 +1,24 @@
 ﻿#
 # TODO: Добавление сертификата для настройки работы winrm по https
-# Скрипт, пока, расчитан только на Windows 10
+# Скрипт, пока, расчитан только на Windows 10 RU
 #
-#
-#
-# gwmi win32_operatingsystem | % caption <- выдаёт версию винды
 
 $Password = "password"
 
-# Устанавливаем choko
-#iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+# Устанавливаем choco
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 # Активируем локального администратора
 Enable-LocalUser Администратор
+
 # Преобразуем пароль
 $EncryptedPassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
 # Устанавливаем пароль локальному администратору
 Set-LocalUser -Name Администратор -Password $EncryptedPassword
 
 # Включаем тихую установку winrm с использованием http
-winrm qc -q -transport:http -f
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
 
-#Enable-PSRemoting -Force 
 #Get-NetConnectionProfile
 #Set-NetConnectionProfile -InterfaceIndex 6 -NetworkCategory Private
